8a87560460782ff104f0c25876860d55fffccdf5
Loi <loia5tqd001@gmail.com>
2019-12-04 12:26:04 +0700

    vuejs-component: menu item 1

---
+++ Frontend/Vue-components/components/menu-items/menu-item-1.vue
@@ -0,0 +1,73 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="nav in navs" :key="nav">
+        <a href="#">
+          <span>{{ nav }}</span>
+          <span>{{ nav }}</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+module.exports = {
+  data() {
+    return {
+      navs: [ 'Home', 'About', 'Services', 'Portfolio', 'Team', 'Contact' ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  * {
+    --width: 60vw;
+    --font-size: 20px;
+    --time-animation: .3s;
+    --colors--background: #333;
+    --colors--nav--normal: #aaa;
+    --colors--nav--hover: #eee;
+
+    margin: 0;
+    padding: 0;
+  }
+  nav {
+    font-family: 'Montserrat', sans-serif;
+    background: var(--colors--background);
+    font-size: var(--font-size);
+    width: var(--width);
+    padding: .3em;
+  }
+  ul {
+    display: flex;
+    justify-content: space-around;
+  }
+  ul li {
+    list-style-type: none;
+    overflow: hidden;
+  }
+  ul li a {
+    display: block;
+    height: 1.3em;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  ul li a span {
+    display: block;
+  }
+  ul li a span:nth-child(1) {
+    color: var(--colors--nav--normal);
+  }
+  ul li a span:nth-child(2) {
+    color: var(--colors--nav--hover);
+  }
+  ul li a {
+    transition: transform var(--time-animation);
+  }
+  ul li:hover a {
+    transform: translateY(calc(-100% + .1em));
+  }
+
+</style>
---
+++ Frontend/Vue-components/main.js
@@ -0,0 +1,6 @@
+new Vue({
+  el: '#app',
+  components: {
+    'menu-item-1': httpVueLoader('./components/menu-items/menu-item-1.vue'),
+  }
+})
